51011c737e62d22abcc24e006647fb1b83ed4852
Burton Smith <burton@breakstuff.io>
2024-01-07 13:06:06 -0500

    initial commit

---
+++ src/additional-svelte-typings.d.ts
@@ -0,0 +1,6 @@
+declare namespace svelteHTML {
+	// enhance elements
+	interface IntrinsicElements {
+		'my-custom-element': { someattribute: string; 'on:event': (e: CustomEvent<any>) => void };
+	}
+}
